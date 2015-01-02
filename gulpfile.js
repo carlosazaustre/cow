defdb99934dcfd987a6143ba1c68a20bb866fa60
carlosazaustre <cazaustre@gmail.com>
2015-01-03 00:48:26 +0100

    added editorconfig and gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var gulp    = require("gulp");
+var mocha   = require("gulp-mocha");
+var jshint  = require("gulp-jshint");
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 "use strict";
 
-var express = require('express');
+var express = require("express");
 var app     = express();
 
 app.listen(3000, function(){
-  console.log('Express App running on http://localhost:3000');
+  console.log("Express App running on http://localhost:3000");
 });
