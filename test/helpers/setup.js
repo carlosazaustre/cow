0557c7b33bc8f1dfe3848ffe422aa10796c04c73
carlosazaustre <cazaustre@gmail.com>
2015-01-03 01:00:03 +0100

    making test enviroment

---
+++ test/helpers/setup.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// Assertions and testing utilities
+var chai            = require('chai');
+var chaiAsPromised  = require('chai-as-promised');
+
+chai.use(chaiAsPromised);
+
+GLOBAL.AssertionError = chai.AssertionError;
+GLOBAL.expect = chai.expect;
