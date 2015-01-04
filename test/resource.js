d87dbbf14e9b9f2fcf5cdd09baa8b31a2d5ab611
carlosazaustre <cazaustre@gmail.com>
2015-01-04 14:09:56 +0100

    add resource test

--- test/resource.js
+++ test/resource.js
@@ -4,3 +4,50 @@ var request   = require('supertest-as-promised');
 var expect    = require('chai').expect;
 var _         = require('lodash');
 var mongoose  = require('mongoose');
+var Resource  = mongoose.model('Resource');
+var api       = require('../index');
+var host      = process.env.API_TEST_HOST || api;
+
+request = request(host);
+
+// -- Tests --------------------------------------------------------------------
+describe('Resources collection [/resource]', function() {
+  describe('POST', function() {
+    //TODO
+  });
+
+  decribe('GET /:id', function() {
+    //TODO
+  });
+
+  describe('GET', function() {
+    //TODO
+  });
+
+  describe('PUT /:id', function() {
+    //TODO
+  });
+
+  describe('DELETE /:id', function() {
+    //TODO
+  });
+});
+
+// -- Private methods ----------------------------------------------------------
+function _createResource() {
+  var id;
+  var data = {
+    'title': 'A new resource'
+  };
+
+  return request
+    .post('/resource')
+    .set('Accept', 'application/json')
+    .send(data)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+  .then(function getResource(res) {
+    this.id = res.body._id,
+    this.resource = res.body
+  }.bind(this));
+};
