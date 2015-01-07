6f541424e2dab17914381bd2d3f221ab6ba4cd28
carlosazaustre <cazaustre@gmail.com>
2015-01-07 16:16:17 +0100

    write test for GET all

--- test/resource.js
+++ test/resource.js
@@ -26,7 +26,7 @@ describe('Resources collection [/resource]', function() {
 
   // -- Tests ------------------------------------------------------------------
 
-  describe('POST', function() {
+  describe('POST /resource', function() {
     it('should be create a resource', function(done) {
       var data = { "title": "A new resource" };
 
@@ -36,17 +36,18 @@ describe('Resources collection [/resource]', function() {
         .send(data)
         .expect(201)
         .expect('Content-Type', /application\/json/)
-        .end(function(err, res) {
-          var body = res.body;
-          // Properties
-          expect(body).to.have.property('title', 'A new resource');
-          expect(body).to.have.property('_id');
-          done(err);
-        });
+      .end(function assertions(err, res) {
+        var body = res.body;
+
+        // Properties
+        expect(body).to.have.property('title', 'A new resource');
+        expect(body).to.have.property('_id');
+        done(err);
+      });
     });
   });
 
-  describe('GET /:id', function() {
+  describe('GET /resource/:id', function() {
     it('should get an existing resource', function(done) {
       var id;
       var data = { "title": "A new resource" };
@@ -57,28 +58,77 @@ describe('Resources collection [/resource]', function() {
         .send(data)
         .expect(201)
         .expect('Content-Type', /application\/json/)
-        .then(function getResource(res) {
-          id = res.body._id;
 
-          return request
-            .get('/resource/' + id)
-            .set('Accept', 'application/json')
-            .send()
-            .expect(200)
-            .expect('Content-Type', /application\/json/)
-        }, done)
-        .then(function assertions(res) {
-          var body = res.body;
-          // Properties
-          expect(body).to.have.property('_id', id);
-          expect(body).to.have.property('title', 'A new resource');
-          done();
-        }, done);
+      .then(function getResource(res) {
+        id = res.body._id;
+
+        return request
+          .get('/resource/' + id)
+          .set('Accept', 'application/json')
+          .send()
+          .expect(200)
+          .expect('Content-Type', /application\/json/)
+      }, done)
+
+      .then(function assertions(res) {
+        var body = res.body;
+        // Properties
+        expect(body).to.have.property('_id', id);
+        expect(body).to.have.property('title', 'A new resource');
+        done();
+      }, done);
     });
   });
 
-  describe('GET', function() {
-    //TODO
+  describe('GET /resource', function() {
+    it('should get all existing resources', function(done) {
+      var id1, id2;
+      var data1 = { "title": "A new resource" },
+          data2 = { "title": "Another resource" };
+
+      request
+        .post('/resource')
+        .set('Accept', 'application/json')
+        .send(data1)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+      .then(function postAnotherResource(res) {
+        id1 = res.body._id
+        return request
+          .post('/resource')
+          .set('Accept', 'application/json')
+          .send(data2)
+          .expect(201)
+          .expect('Content-Type', /application\/json/)
+      })
+
+      .then(function getResources(res) {
+        id2 = res.body._id;
+        return request
+          .get('/resource')
+          .set('Accept', 'application/json')
+          .expect(200)
+          .expect('Content-Type', /application\/json/)
+      }, done)
+
+      .then(function assertions(res) {
+        var resources = res.body;
+
+        expect(resources).to.be.an('array')
+          .and.to.have.lenght.above(2);
+
+        var resource1 = _.find(resources, { id: id1 });
+        var resource2 = _.find(resources, { id: id2 });
+
+        // Properties
+        expect(resource1).to.have.property('id', id1);
+        expect(resource1).to.have.property('title', 'A new resource');
+        expect(resource2).to.have.property('id', id2);
+        expect(resource2).to.have.property('title', 'Another resource');
+        done();
+      }, done);
+    });
   });
 
   describe('PUT /:id', function() {
